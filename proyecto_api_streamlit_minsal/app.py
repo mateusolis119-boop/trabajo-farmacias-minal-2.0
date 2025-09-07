29ad1df888884f8d4eb7d9161c631c8d05f8cd51
mateusolis119-boop <mateusolis119@gmail.com>
2025-09-07 18:07:20 -0300

    Update app.py

--- proyecto_api_streamlit_minsal/app.py
+++ proyecto_api_streamlit_minsal/app.py
@@ -2,9 +2,12 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from api_client import get_locales, get_turnos
-from analysis import to_dataframe, coerce_types, resumen_basico, top_por_categoria, abrir_vs_cerrar_promedio
-from charts import bar_top, line_by_date
+from proyecto_api_streamlit_minsal.api_client import get_locales, get_turnos
+from proyecto_api_streamlit_minsal.analysis import (
+    to_dataframe, coerce_types, resumen_basico, top_por_categoria, abrir_vs_cerrar_promedio
+)
+from proyecto_api_streamlit_minsal.charts import bar_top, line_by_date
+
 
 st.set_page_config(page_title="Farmacias MINSAL - Análisis API", layout="wide")
 st.title("Análisis y presentación de datos utilizando APIs en Python")
